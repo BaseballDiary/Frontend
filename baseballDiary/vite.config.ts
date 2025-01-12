fc7e2edb6827c99ad36494665f79887a97c8dfc2
임동재 <djlim1998@gmail.com>
2025-01-12 23:25:30 +0900

    #2 feature: Layout파일 추가
    - Navbar 사용을 위해 추가

--- baseballDiary/vite.config.ts
+++ baseballDiary/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // 브라우저 자동 열기
+    watch: {
+      usePolling: true, // 파일 변경 감지
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
 });
